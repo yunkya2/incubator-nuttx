f4714497b07da976091a64287b255757d9ab2719
Yuichi Nakamura <y.512.nakamura@gmail.com>
2021-02-18 16:35:25 +0900

    update memorymap

--- arch/arm/src/rp2040/hardware/rp2040_memorymap.h
+++ arch/arm/src/rp2040/hardware/rp2040_memorymap.h
@@ -21,9 +21,9 @@
 #ifndef __ARCH_ARM_SRC_RP2040_HARDWARE_RP2040_MEMORYMAP_H
 #define __ARCH_ARM_SRC_RP2040_HARDWARE_RP2040_MEMORYMAP_H
 
-/********************************************************************************************
+/****************************************************************************
  * Included Files
- ********************************************************************************************/
+ ****************************************************************************/
 
 #include <nuttx/config.h>
 
@@ -31,15 +31,15 @@
 
 #include "chip.h"
 
-/********************************************************************************************
+/****************************************************************************
  * Pre-processor Definitions
- ********************************************************************************************/
+ ****************************************************************************/
 
 #define RP2040_FLASH_BASE                0x10000000 /* -0x001fffff: FLASH memory space (2048KB) */
 #define RP2040_SRAM_BASE                 0x20000000 /* -0x20041fff: SRAM memory space (264KB) */
 
 #define RP2040_XIP_CTRL_BASE             0x14000000  /* QSPI flash execute-in-place block */
-#define RP2040_XIP_SSI_BASE              0x18000000  /* DW_apb_ssi has the following features: * APB interface – Allows for easy integration into a DesignWare Synthesizable Components for AMBA 2 implementation. * APB3 and APB4 protocol support. * Scalable APB data bus width – Supports APB data bus widths of 8, 16, and 32 bits. * Serial-master or serial-slave operation – Enables serial communication with serial-master or serial-slave peripheral devices. * Programmable Dual/Quad/Octal SPI support in Master Mode. * Dual Data Rate (DDR) and Read Data Strobe (RDS) Support - Enables the DW_apb_ssi master to perform operations with the device in DDR and RDS modes when working in Dual/Quad/Octal mode of operation. * Data Mask Support - Enables the DW_apb_ssi to selectively update the bytes in the device. This feature is applicable only in enhanced SPI modes. * eXecute-In-Place (XIP) support - Enables the DW_apb_ssi master to behave as a memory mapped I/O and fetches the data from the device based on the APB read request. This feature is applicable only in enhanced SPI modes. * DMA Controller Interface – Enables the DW_apb_ssi to interface to a DMA controller over the bus using a handshaking interface for transfer requests. * Independent masking of interrupts – Master collision, transmit FIFO overflow, transmit FIFO empty, receive FIFO full, receive FIFO underflow, and receive FIFO overflow interrupts can all be masked independently. * Multi-master contention detection – Informs the processor of multiple serial-master accesses on the serial bus. * Bypass of meta-stability flip-flops for synchronous clocks – When the APB clock (pclk) and the DW_apb_ssi serial clock (ssi_clk) are synchronous, meta-stable flip-flops are not used when transferring control signals across these clock domains. * Programmable delay on the sample time of the received serial data bit (rxd); enables programmable control of routing delays resulting in higher serial data-bit rates. * Programmable features: - Serial interface operation – Choice of Motorola SPI, Texas Instruments Synchronous Serial Protocol or National Semiconductor Microwire. - Clock bit-rate – Dynamic control of the serial bit rate of the data transfer; used in only serial-master mode of operation. - Data Item size (4 to 32 bits) – Item size of each data transfer under the control of the programmer. * Configured features: - FIFO depth – 16 words deep. The FIFO width is fixed at 32 bits. - 1 slave select output. - Hardware slave-select – Dedicated hardware slave-select line. - Combined interrupt line - one combined interrupt line from the DW_apb_ssi to the interrupt controller. - Interrupt polarity – active high interrupt lines. - Serial clock polarity – low serial-clock polarity directly after reset. - Serial clock phase – capture on first edge of serial-clock directly after reset. */
+#define RP2040_XIP_SSI_BASE              0x18000000
 #define RP2040_SYSINFO_BASE              0x40000000
 #define RP2040_SYSCFG_BASE               0x40004000  /* Register block for various chip control signals */
 #define RP2040_CLOCKS_BASE               0x40008000
@@ -78,33 +78,28 @@
 #define RP2040_ATOMIC_SET_REG_OFFSET         0x2000
 #define RP2040_ATOMIC_CLR_REG_OFFSET         0x3000
 
- /********************************************************************************************
+/****************************************************************************
  * Public Types
- ********************************************************************************************/
+ ****************************************************************************/
 
-/********************************************************************************************
+/****************************************************************************
  * Public Data
- ********************************************************************************************/
+ ****************************************************************************/
 
-/************************************************************************************
+/****************************************************************************
  * Inline Functions
- ************************************************************************************/
+ ****************************************************************************/
 
 #ifndef __ASSEMBLY__
 
-# define xorbits_reg32(v,a)  putreg32(v, (a) | RP2040_ATOMIC_XOR_REG_OFFSET)
-# define setbits_reg32(v,a)  putreg32(v, (a) | RP2040_ATOMIC_SET_REG_OFFSET)
-# define clrbits_reg32(v,a)  putreg32(v, (a) | RP2040_ATOMIC_CLR_REG_OFFSET)
+# define xorbits_reg32(v,a)   putreg32(v, (a) | RP2040_ATOMIC_XOR_REG_OFFSET)
+# define setbits_reg32(v,a)   putreg32(v, (a) | RP2040_ATOMIC_SET_REG_OFFSET)
+# define clrbits_reg32(v,a)   putreg32(v, (a) | RP2040_ATOMIC_CLR_REG_OFFSET)
+# define modbits_reg32(v,m,a) xorbits_reg32((getreg32(a) ^ (v)) & (m), a)
 
-inline void modbits_reg32(uint32_t values, uint32_t mask, uint32_t addr)
-{
-  uint32_t a = (getreg32(addr) ^ values) & mask;
-  xorbits_reg32(a, addr);
-}
-
-/************************************************************************************
+/****************************************************************************
  * Public Function Prototypes
- ************************************************************************************/
+ ****************************************************************************/
 #undef EXTERN
 #if defined(__cplusplus)
 #define EXTERN extern "C"
@@ -113,7 +108,6 @@ extern "C"
 #else
 #define EXTERN extern
 #endif
-
 
 #undef EXTERN
 #if defined(__cplusplus)
